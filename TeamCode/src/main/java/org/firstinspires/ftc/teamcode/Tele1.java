703a532a026ac365f4dd4bd4a5894a1ceedf6ea6
Sho <kiami.sho@gmail.com>
2019-11-19 17:30:41 -0800

    bruh

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele1.java
@@ -31,8 +31,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="Tele1", group="Iterative Opmode")
