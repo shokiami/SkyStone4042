4f9121398be5289bf9f1606789d0a4e5ecbbf1ce
Sho <kiami.sho@gmail.com>
2019-12-17 16:53:48 -0800

    gyro stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gyro.java
@@ -14,36 +14,28 @@ public class Gyro {
 
     Gyro(HardwareMap hardwareMap) {
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-
         parameters.mode = BNO055IMU.SensorMode.IMU;
         parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
         parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
         parameters.loggingEnabled = false;
-
         gyro = hardwareMap.get(BNO055IMU.class, "imu");
         gyro.initialize(parameters);
     }
 
     public double getAngle () {
         Orientation angles = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-
         double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
-
         if (deltaAngle < -180)
             deltaAngle += 360;
         else if (deltaAngle > 180)
             deltaAngle -= 360;
-
         globalAngle += deltaAngle;
-
         lastAngles = angles;
-
         return globalAngle;
     }
 
     public void resetAngle () {
         lastAngles = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-
         globalAngle = 0;
     }
 }
