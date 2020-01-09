8706e580fcbc539e6d079a814506fe19376f48d5
jack.roedel@hotmail.com <hanro0311>
2020-01-09 15:59:36 -0800

    remade the 180 spin feature, still not tested so idk if it works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele1.java
@@ -37,13 +37,17 @@ public class Tele1 extends OpMode {
     //Declare OpMode members
     Robot robot;
     Controller controller1;
+    Controller controller2;
+    Robot.RotateListener rotateListener;
 
     //Code to run ONCE when the driver hits INIT
     @Override
     public void init() {
         robot = new Robot(hardwareMap, false);
         controller1 = new Controller(gamepad1);
+        controller2 = new Controller(gamepad2);
         telemetry.addData("Status", "Initialized");
+        rotateListener.start();
     }
 
     //Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
@@ -62,6 +66,7 @@ public class Tele1 extends OpMode {
     @Override
     public void loop() {
         controller1.update();
+        controller2.update();
 
         //Speed
         if (controller1.x.equals("pressing")) {
@@ -74,39 +79,39 @@ public class Tele1 extends OpMode {
         robot.strafePower = 0.5 * controller1.right_stick_x + 0.5 * controller1.left_stick_x;
         robot.updateBallDrive();
 
+        //Spin 180
+        if (controller1.left_bumper.equals("pressing")) {
+            rotateListener.setRunning(true);
+        }
+
         //Lift
-        if (controller1.dpad_right.equals("pressed")) {
+        if (controller2.dpad_right.equals("pressed")) {
             robot.tuneLift(0.002);
         }
-        if (controller1.dpad_left.equals("pressed")) {
+        if (controller2.dpad_left.equals("pressed")) {
             robot.tuneLift(-0.002);
         }
-        if (controller1.dpad_up.equals("pressing") && robot.liftHeight < 6) {
+        if (controller2.dpad_up.equals("pressing") && robot.liftHeight < 6) {
             robot.liftHeight += 1;
             robot.updateLift();
-        } else if (controller1.dpad_down.equals("pressing") && robot.liftHeight > 0) {
+        } else if (controller2.dpad_down.equals("pressing") && robot.liftHeight > 0) {
             robot.liftHeight -= 1;
             robot.updateLift();
         }
 
         //Intake
-        if (controller1.right_bumper.equals("pressing")) {
+        if (controller2.right_bumper.equals("pressing")) {
             robot.toggleIntake();
         }
 
         //Hook
-        if (controller1.a.equals("pressing")) {
+        if (controller2.a.equals("pressing")) {
             robot.toggleHook();
         }
 
         //Valve (not in use)
-        if (controller1.left_bumper.equals("pressing")) {
+        if (controller2.left_bumper.equals("pressing")) {
             robot.toggleValve();
-        }
-
-        //Spin 180
-        if (controller1.left_bumper.equals("pressing")) {
-            robot.rotate(180);
         }
 
         telemetry.addData("liftMotor", "" + robot.liftMotor.getCurrentPosition());
