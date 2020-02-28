f9d6503d0775f8db1c0753644d92ee1924240ead
ghs-robotics <contact@garfieldrobotics.com>
2020-02-27 17:33:29 -0800

    Some minor edits + start of TwoStoneAuto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleControllerTele.java
@@ -0,0 +1,119 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="SingleControllerTele", group="Iterative Opmode")
+public class SingleControllerTele extends OpMode {
+    //Declare OpMode members
+    Robot robot;
+    Controller controller1;
+    boolean gyroMode = true;
+
+    //Code to run ONCE when the driver hits INIT
+    @Override
+    public void init() {
+        robot = new Robot(hardwareMap, telemetry, false);
+        controller1 = new Controller(gamepad1);
+        telemetry.addData("Status", "Initialized");
+    }
+
+    //Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+    @Override
+    public void init_loop() {
+    }
+
+    //Code to run ONCE when the driver hits PLAY
+    @Override
+    public void start() {
+        robot.resetElapsedTime();
+        robot.toggleIntakeAngle();
+    }
+
+    //Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+    @Override
+    public void loop() {
+        controller1.update();
+
+        //Speed
+        if (controller1.x.equals("pressing")) {
+            robot.toggleSpeed();
+        }
+
+        //Ball Drive (POV)
+        robot.leftPower = Range.clip(controller1.left_stick_y + controller1.right_stick_x, -1.0, 1.0);
+        robot.rightPower = Range.clip(controller1.left_stick_y - controller1.right_stick_x, -1.0, 1.0);
+        robot.strafePower = controller1.left_stick_x;
+
+        if ((controller1.left_stick_y != 0 || controller1.left_stick_x != 0) && controller1.right_stick_x == 0) {
+            robot.updateBallDrive(gyroMode);
+        } else {
+            robot.updateBallDrive(false);
+            robot.targetAngle = robot.getGyroAngle();
+        }
+
+        if (controller1.right_bumper.equals("pressing")) {
+            gyroMode = true;
+        }
+        if (controller1.left_bumper.equals("pressing")) {
+            gyroMode = false;
+        }
+
+        //Lift
+        if (controller1.dpad_up.equals("pressed")) {
+            robot.liftMotor.setPower(1);
+        } else if (controller1.dpad_down.equals("pressed")) {
+            robot.liftMotor.setPower(-0.3);
+        } else {
+            robot.liftMotor.setPower(0);
+        }
+
+        //Intake
+        if (controller1.right_bumper.equals("pressing")) {
+            robot.intakeMotor.setPower(1);
+        }
+        if (controller1.left_bumper.equals("pressing")) {
+            robot.intakeMotor.setPower(0);
+        }
+
+        //Hook
+        if (controller1.y.equals("pressing")) {
+            robot.toggleHook();
+        }
+    }
+
+    //Code to run ONCE after the driver hits STOP
+    @Override
+    public void stop () {
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoStoneAuto.java
@@ -0,0 +1,119 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="TwoStoneAuto", group="Linear Opmode")
+public class TwoStoneAuto extends LinearOpMode {
+    //Declare OpMode members
+    Robot robot;
+
+    @Override
+    public void runOpMode() {
+        //Code to run ONCE when the driver hits INIT
+        robot = new Robot(hardwareMap, telemetry, true);
+        telemetry.addData("Status", "Initialized");
+
+        waitForStart();
+
+        //Code to run ONCE when the driver hits PLAY
+        robot.resetElapsedTime();
+        robot.toggleIntakeAngle();
+        robot.toggleHook();
+        robot.wait(0.5);
+        robot.toggleLift();
+        robot.toggleLift();
+
+        //Aight, here we go!
+
+        //Locating SkyStone configuration
+        int stoneConfig; //(001 001) = 0; (010 010) = 1; (100 100) = 2
+        robot.move(18, 0);
+        robot.wait(0.5);
+        if (robot.isTargetVisible()) {
+            stoneConfig = 1;
+        } else {
+            stoneConfig = 0;
+            robot.move(0, 8);
+        }
+
+        //Sucking SkyStone 1
+        robot.toggleIntake();
+        robot.move(10, 0);
+        robot.move(-12, 0);
+
+        //Traversing Map
+        robot.move(0, 104 + 8 * stoneConfig);
+
+        //Dropping off SkyStone 1
+        robot.toggleLift(); // lift up
+        robot.speed = 0.7;
+        robot.move(13, 0);
+        robot.toggleIntake();
+        robot.toggleHook(); // hook down
+        robot.wait(0.4);
+
+        //Pulling foundation back into build site
+        robot.speed = 1.0;
+        robot.move(-32, 0);
+        robot.toggleHook(); //hook up
+
+        //Parking under the bridge
+        robot.move(1, -30);
+        robot.toggleLift();
+        robot.move(1, -22);
+
+        //Nice!
+
+        /*
+
+        //Traversing Map
+        robot.move(0, -64 - 8 * stoneConfig);
+
+        //Sucking SkyStone 2
+        robot.toggleIntake();
+        robot.move(11, 0);
+        robot.move(-11, 0);
+
+        //Traversing Map
+        robot.move(0, 64 + 8 * stoneConfig);
+
+        //Dropping off SkyStone 2
+        robot.speed = 0.2;
+        robot.toggleLift();
+        robot.toggleIntake();
+        robot.move(8, 0);
+
+        */
+    }
+}
+
